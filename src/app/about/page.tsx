a7c3572c002e5fabb5746ad8db1d9dab1a4fc027
Victor Huynh <vichuynh6@gmail.com>
2024-02-05 23:21:50 -0500

    updated favicon, added about page, removed t3 scaffolding home page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <main className="">
+            <h1>My life story</h1>
+            <div>
+                <span>
+                    We&apos;re back.
+                </span>
+            </div>
+        </main>
+    );
+}
