75421ae53f5a552cc0ba0bbedfb3257dfc869622
Victor Huynh <vichuynh6@gmail.com>
2024-04-02 20:31:45 -0400

    added dark mode toggle and basic description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,9 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
+      <head />
       <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <ThemeProvider attribute="class">
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,46 @@
-import { unstable_noStore as noStore } from "next/cache";
+"use client"
+
+import { useTheme } from "next-themes";
+// import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 
-// import { CreatePost } from "~/app/_components/create-post";
-// import { getServerAuthSession } from "~/server/auth";
-// import { api } from "~/trpc/server";
+export default function HomePage() {
+  const { theme, setTheme } = useTheme();
 
-export default async function HomePage() {
   return (
-    <main className="bg-slate-500">
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+    <main>
+      <div className="w-full h-20 bg-emerald-800 dark:bg-slate-900 sticky top-0">
         <nav className="flex justify-between items-center h-full">
+          <ul className="flex text-white">
+            <li>
+              <Link href="/">
+                Victor Huynh
+              </Link>
+            </li>
+          </ul>
           <ul className="flex gap-x-6 text-white">
             <li>
-              <Link href="/">Home</Link>
+              <Link href="/about">About</Link>
             </li>
             <li>
-              <Link href="/about">About</Link>
+              <Link href="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li>
+              <div>
+                <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>Toggle Theme</button>
+              </div>
             </li>
           </ul>
         </nav>
       </div>
       <div>
-        Victor Huynh
+        <span className="flex justify-center">
+          Hello my name is Victor, I&apos;m a gamerz, athlete, and software engineer.
+          Historically, I&apos;ve always been a gamer first kind of guy, and that is why I&apos;ve always wanted to build a game of my own.
+        </span>
       </div>
     </main>
   );
